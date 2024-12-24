de9dc1445104e139b555331fd4c26f7f90ff8cb9
srinivas <srinivas@vishal.>
2024-12-24 13:38:16 +0000

    change app test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and buddy, hello Bye.
+          Edit <code>src/App.js</code> I was change to dev branch.
         </p>
         <a
           className="App-link"
